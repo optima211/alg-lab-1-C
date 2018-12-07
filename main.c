8f6a9c7d4314f4218d83d22d4772ac6da2792056
optima211 <nedmk@mail.ru>
2018-12-07 22:44:05 +0700

    add pyramid sort method & correct user interface

--- main.c
+++ main.c
@@ -42,6 +42,44 @@ void shellSort(int *num, int size) {
     }
 }
 
+// Функция "просеивания" через кучу - формирование кучи (включена в пирамидальную сортировку)
+void siftDown(int *numbers, int root, int bottom) {
+    int maxChild; // индекс максимального потомка
+    int done = 0; // флаг того, что куча сформирована
+    // Пока не дошли до последнего ряда
+    while ((root * 2 <= bottom) && (!done)) {
+        if (root * 2 == bottom)    // если мы в последнем ряду,
+            maxChild = root * 2;    // запоминаем левый потомок
+            // иначе запоминаем больший потомок из двух
+        else if (numbers[root * 2] > numbers[root * 2 + 1])
+            maxChild = root * 2;
+        else
+            maxChild = root * 2 + 1;
+        // если элемент вершины меньше максимального потомка
+        if (numbers[root] < numbers[maxChild]) {
+            int temp = numbers[root]; // меняем их местами
+            numbers[root] = numbers[maxChild];
+            numbers[maxChild] = temp;
+            root = maxChild;
+        } else // иначе
+            done = 1; // пирамида сформирована
+    }
+}
+
+// Функция пирамидальной сортировки
+void PyramidSort(int *numbers, int array_size) {
+    // Формируем нижний ряд пирамиды
+    for (int i = (array_size / 2) - 1; i >= 0; i--)
+        siftDown(numbers, i, array_size - 1);
+    // Просеиваем через пирамиду остальные элементы
+    for (int i = array_size - 1; i >= 1; i--) {
+        int temp = numbers[0];
+        numbers[0] = numbers[i];
+        numbers[i] = temp;
+        siftDown(numbers, 0, i - 1);
+    }
+}
+
 int main() {
     int a[10]; // Объявляем массив из 10 элементов
     int select;
@@ -53,6 +91,7 @@ int main() {
     }
     printf("Select function of sort you massive:\n 1- Method direct sort\n 2- Method of Shell\n 3- Method of pyramidal sort\n");
     scanf_s("%d", &select);
+    //меню выбора сортирвки
     switch (select) {
         case 1:
             selectionSort(a, 10);  // вызываем функцию сортировки прямым выбором
@@ -61,6 +100,10 @@ int main() {
         case 2:
             shellSort(a, 10); // вызываем функцию сортировки методом Шелла
             printf("Array output after applying the Shell sort method:\n");
+            break;
+        case 3:
+            PyramidSort(a, 10); // вызов функции пирамидальной сортировки
+            printf("Array output after applying the pyramid sort method:\n");
             break;
         default:
             printf("restart the program and enter the correct value\n");
@@ -71,7 +114,7 @@ int main() {
         // Выводим отсортированные элементы массива
         for (int i = 0; i < 10; i++)
             printf("%d ", a[i]);
-        printf("\nAll OK?\n Please, press button 'Enter' on you keyboard!\n Thanks!\n\n");
+        printf("\nAll work done!\n Please, press button 'Enter' on you keyboard!\n Thanks!\n\n");
     }
     getchar();
     getchar();
